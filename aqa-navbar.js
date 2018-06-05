1a8f62853fa932da9cdd01e7d95eb9d642be6570
Tonsai jompun <tonsai.j@nexts-corp.com>
2018-06-05 13:47:51 +0700

    update @nylon

--- aqa-navbar.js
+++ aqa-navbar.js
@@ -1,5 +1,5 @@
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-import '@aqa/aqa-font/aqa-font.js';
+import '@nylon/aqa-font/aqa-font.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 /**
  * `aqa-navbar`
